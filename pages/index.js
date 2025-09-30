0a53e0fa2b058f1e07220e3aee0fef1720b2c4e8
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2025-09-30 12:34:38 +0000

    adicionar scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Teste</h1>
+function Home() {
+  return <h1>Teste</h1>;
 }
 
-export default Home
+function teste() {
+  console.log("teste");
+}
+
+function testePrettier() {
+  console.log();
+}
+
+export default Home;
